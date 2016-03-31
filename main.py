441e1955cb36396e7dcb78d4b7ade25f5d3fb235
glebmish <bboygleb@gmail.com>
2016-03-31 17:21:38 +0300

    Made sross-validation

--- main.py
+++ main.py
@@ -8,8 +8,21 @@ def guess():
         print BC.classify(name)
         name = raw_input("Write surname: ")
 
-train, test = divide(DataSet("dataset_surnames.txt"), 10, 20)
+def cross_validate(classifier, dataset):
+    recall = 0.0
+    precision = 0.0
+    for i in range(10):
+        train, test = divide(dataset, i*10, i*10 + 10)
+        BC.train(train)
+        cur_recall, cur_precision = BC.check(test)
+        recall += cur_recall
+        precision += cur_precision
+    recall /= 10
+    precision /= 10
+    return recall, precision
+
+dataset = DataSet("dataset_surnames.txt")
 BC = BayesClassifier(lambda x: {'last': x[-1], '2nd-last': x[-2], '3rd-last': x[-3]})
-BC.train(train)
-print zip(['recall:', 'precision:'], BC.check(test))
+print zip(['recall', 'precision'], cross_validate(BC, dataset))
+BC.train(dataset)
 guess()
